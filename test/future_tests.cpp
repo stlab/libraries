2d9f0841e7a449950b45151ec46e279a323188ba
Felix Petriconi <felix@petriconi.net>
2016-03-04 22:49:05 +0100

    Adding first continuation tests

--- test/future_tests.cpp
+++ test/future_tests.cpp
@@ -8,36 +8,48 @@ Distributed under the Boost Software License, Version 1.0.
 
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include <boost/mpl/list.hpp>
+
 #include <stlab/future.hpp>
 
 using namespace stlab;
 
-auto create_void_future() {
-    return future<void>();
-}
+using all_test_types = boost::mpl::list<void, int, std::unique_ptr<int>>;
 
-auto create_non_void_future() {
-    return future<int>();
-}
-
-auto create_movable_only_future() {
-    return future<std::unique_ptr<int>>();
-}
-
-BOOST_AUTO_TEST_CASE(future_default_constructed_void) {
-    auto sut = create_void_future();
+BOOST_AUTO_TEST_CASE_TEMPLATE(future_default_constructed, T, all_test_types)
+{
+    auto sut = future<T>();
     BOOST_REQUIRE(sut.valid() == false);
     BOOST_REQUIRE(sut.cancel_try() == false);
 }
 
-BOOST_AUTO_TEST_CASE(future_default_constructed_non_void) {
-    auto sut = create_non_void_future();
-    BOOST_REQUIRE(sut.valid() == false);
+using copyable_test_types = boost::mpl::list<void, int>;
+
+BOOST_AUTO_TEST_CASE_TEMPLATE(future_constructed_minimal_fn, T, copyable_test_types) {
+    auto sut = async(default_scheduler(), std::function<T()>());
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
     BOOST_REQUIRE(sut.cancel_try() == false);
+    BOOST_REQUIRE(sut.valid() == true);
 }
 
-BOOST_AUTO_TEST_CASE(future_default_constructed_move_only) {
-    auto sut = create_movable_only_future();
-    BOOST_REQUIRE(sut.valid() == false);
+#if 0
+BOOST_AUTO_TEST_CASE(future_constructed_minimal_fn_moveonly) {
+    auto sut = async(default_scheduler(), []()->std::unique_ptr<int> { 
+        auto r = std::make_unique<int>(); 
+        *r = 42; 
+        return std::move(r); 
+    });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try()) {}
+    auto result = sut.get_try();
+    
+    BOOST_REQUIRE(**result == 42);
     BOOST_REQUIRE(sut.cancel_try() == false);
+    BOOST_REQUIRE(sut.valid() == true);
 }
+
+#endif
--- test/future_then_tests.cpp
+++ test/future_then_tests.cpp
@@ -9,3 +9,117 @@ Distributed under the Boost Software License, Version 1.0.
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <stlab/future.hpp>
+
+using namespace stlab;
+
+namespace
+{
+    struct custom_scheduler {
+        using result_type = void;
+
+        template <typename F>
+        void operator()(F f) {
+            ++_usage_counter;
+            detail::async_(std::move(f));
+        }
+
+        static int usage_counter() { return _usage_counter; }
+
+        static void reset_usage_counter() { _usage_counter = 0; }
+
+    private:
+        static int _usage_counter;
+    };
+
+    int custom_scheduler::_usage_counter = 0;
+
+    struct test_setup
+    {
+        test_setup() {
+            custom_scheduler::reset_usage_counter();
+        }
+    };
+}
+
+BOOST_AUTO_TEST_CASE(future_void_single_task) {
+    test_setup setup;
+    int p = 0;
+
+    auto sut = async(custom_scheduler(), [&p] { p = 42; });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try());
+    
+    BOOST_REQUIRE_EQUAL(42, p);
+    BOOST_REQUIRE_EQUAL(1, custom_scheduler::usage_counter());
+}
+
+BOOST_AUTO_TEST_CASE(future_void_two_tasks) {
+    test_setup setup;
+    std::atomic_int p = 0;
+
+    auto sut = async(custom_scheduler(), [_p = &p] { *_p = 42; }).then([_p = &p]{ *_p += 42; });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try());
+    
+    BOOST_REQUIRE_EQUAL(42 + 42, p);
+    BOOST_REQUIRE_EQUAL(2, custom_scheduler::usage_counter());
+}
+
+
+BOOST_AUTO_TEST_CASE(future_int_single_task) {
+    test_setup setup;
+
+    auto sut = async(custom_scheduler(), [] { return 42; });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try());
+
+    BOOST_REQUIRE_EQUAL(42, *sut.get_try());
+    BOOST_REQUIRE_EQUAL(1, custom_scheduler::usage_counter());
+}
+
+BOOST_AUTO_TEST_CASE(future_int_two_tasks) {
+    test_setup setup;
+
+    auto sut = async(custom_scheduler(), [] { return 42; }).then([](auto x) { return x + 42; });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try());
+
+    BOOST_REQUIRE_EQUAL(42 + 42, *sut.get_try());
+    BOOST_REQUIRE_EQUAL(2, custom_scheduler::usage_counter());
+}
+
+BOOST_AUTO_TEST_CASE(future_int_void_two_tasks) {
+    test_setup setup;
+    std::atomic_int p = 0;
+
+    auto sut = async(custom_scheduler(), [_p = &p] { return 42; }).then([_p = &p](auto x) { *_p = x + 42; });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try());
+
+    BOOST_REQUIRE_EQUAL(42 + 42, p);
+    BOOST_REQUIRE_EQUAL(2, custom_scheduler::usage_counter());
+}
+
+BOOST_AUTO_TEST_CASE(future_void_int_two_tasks) {
+    test_setup setup;
+    std::atomic_int p = 0;
+
+    auto sut = async(custom_scheduler(), [_p = &p] { *_p = 42; }).then([_p = &p] { *_p += 42; return _p->load(); });
+    BOOST_REQUIRE(sut.valid() == true);
+    BOOST_REQUIRE(sut.error().is_initialized() == false);
+
+    while (!sut.get_try());
+
+    BOOST_REQUIRE_EQUAL(42 + 42, p);
+    BOOST_REQUIRE_EQUAL(2, custom_scheduler::usage_counter());
+}
