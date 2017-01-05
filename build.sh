57a0680b4adb830cbeec44ec7167790d80f6ac23
Felix Petriconi <felix@petriconi.net>
2017-01-05 14:42:21 +0100

    Try to get clang compilable and gcc terminate

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@ mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_CXX_COMPILER=$COMPILER -DBOOST_ROOT=boost_libraries ./..
 make CXX=$COMPILER -j3
-test/future_test
+test/future_test --log_level=message
+echo all done
