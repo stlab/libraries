b19317b02a40327e2b85faa099d48fc0b9e67a01
Felix Petriconi <felix@petriconi.net>
2017-01-17 21:22:35 +0100

    Update copyright notice to be compliant with STLab policy

--- stlab/tuple_algorithm.hpp
+++ stlab/tuple_algorithm.hpp
@@ -1,7 +1,8 @@
-/*-
-Copyright 2016 Felix Petriconi
-Distributed under the Boost Software License, Version 1.0.
-(See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+/*
+    Copyright 2015 Adobe
+    Distributed under the Boost Software License, Version 1.0.
+    (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+    Author: Felix Petriconi
 */
 
 /**************************************************************************************************/
