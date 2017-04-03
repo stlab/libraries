eca59d5780ef4b8dea3687a89d58e89b7a39514c
Felix Petriconi <felix@petriconi.net>
2017-04-03 10:36:09 +0200

    Adding the concatenation of two channels

--- test/channel_tests.cpp
+++ test/channel_tests.cpp
@@ -198,6 +198,29 @@ BOOST_FIXTURE_TEST_SUITE(int_channel, channel_test_fixture_int_1)
 
         BOOST_REQUIRE_EQUAL(42, result);
     }
+
+    BOOST_AUTO_TEST_CASE(int_concatenate_two_channels) {
+        stlab::sender<int> A, X;
+        stlab::receiver<int> B, Y;
+        std::tie(A, B) = stlab::channel<int>(stlab::default_executor);
+        std::tie(X, Y) = stlab::channel<int>(stlab::default_executor);
+
+        auto b = B | [](int x) { return x * 2; } | X;
+
+        std::atomic_int val{ 0 };
+
+        auto y = Y | [](int v) { return v + 2; } | [&val](int v) { val = v; };
+
+        B.set_ready();
+        Y.set_ready();
+
+        A(20);
+
+        while (!val.load()) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        }
+        BOOST_REQUIRE_EQUAL(42, val.load());
+    }
 BOOST_AUTO_TEST_SUITE_END()
 
 
