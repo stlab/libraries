c3f4d988c8b1f24bbef068ad2625d120710bcbc3
Foster Brereton <fbrereto@adobe.com>
2017-06-07 11:17:11 -0700

    using task instead of function

--- stlab/concurrency/serial_queue.hpp
+++ stlab/concurrency/serial_queue.hpp
@@ -27,18 +27,90 @@ namespace stlab {
 inline namespace v1 {
 
 /**************************************************************************************************/
+// Task are fully captured functions with a mutable call operator to support moving items through.
+
+/*
+    REVISIT (sparent) : Add small object optimizations and reference wrapper support to match
+    std::function. Support arguments?
+*/
+class task {
+    struct concept {
+        virtual ~concept() { }
+        virtual void invoke() = 0;
+        virtual const std::type_info& target_type() const = 0;
+        virtual void* pointer() = 0;
+        virtual const void* pointer() const = 0;
+    };
+
+    template <class F>
+    struct model : concept {
+        model(F&& f) : _f(std::move(f)) { }
+        void invoke() override { _f(); }
+        const std::type_info& target_type() const override { return typeid(F); }
+        void* pointer() override { return &_f; }
+        const void* pointer() const override { return &_f; }
+
+        F _f;
+    };
+
+    std::unique_ptr<concept> _self;
+
+public:
+    using result_type = void;
+
+    task() noexcept = default;
+    task(std::nullptr_t) : task() { }
+    task(const task&) = delete;
+    task(task&&) noexcept = default;
+    template <class F>
+    task(F&& f) : _self(std::make_unique<model<F>>(std::forward<F>(f))) { }
+
+    ~task() noexcept = default;
+
+    task& operator=(const task&) = delete;
+    task& operator=(task&&) noexcept = default;
+    task& operator=(std::nullptr_t) noexcept { _self.reset(); return *this; }
+    template <class F>
+    task& operator=(F&& f) { return *this = task(f); }
+    // REVISIT (sparent) : Support reference wrapper assignment
+
+    void swap(task& x) { std::swap(_self, x._self); }
+
+    explicit operator bool() const { return static_cast<bool>(_self); }
+
+    const std::type_info& target_type() const
+    { return _self ? _self->target_type() : typeid(void); }
+
+    template <class T>
+    T* target() { return (target_type() == typeid(T)) ? _self->pointer() : nullptr; }
+    template <class T>
+    const T* target() const { return (target_type() == typeid(T)) ? _self->pointer() : nullptr; }
+
+
+    void operator()() { _self->invoke(); }
+};
+
+inline void swap(task& x, task& y) { return x.swap(y); }
+inline bool operator==(const task& x, std::nullptr_t) { return !static_cast<bool>(x); }
+inline bool operator==(std::nullptr_t, const task& x) { return !static_cast<bool>(x); }
+inline bool operator!=(const task& x, std::nullptr_t) { return static_cast<bool>(x); }
+inline bool operator!=(std::nullptr_t, const task& x) { return static_cast<bool>(x); }
+
+/**************************************************************************************************/
 
 namespace detail {
 
 /**************************************************************************************************/
 
-class serial_instance_t {
-    typedef std::function<void ()> work_t;
+using executor_t = std::function<void (task&&)>;
 
-    std::function<void (work_t&&)> _executor;
-    std::deque<work_t>             _queue;
-    std::mutex                     _m;
-    bool                           _running{false};
+/**************************************************************************************************/
+
+class serial_instance_t {
+    executor_t       _executor;
+    std::deque<task> _queue;
+    std::mutex       _m;
+    bool             _running{false};
 
     struct set_bool_t {
         set_bool_t(bool& b) noexcept : _b(b) { _b = true; }
@@ -64,7 +136,7 @@ class serial_instance_t {
             return;
         }
 
-        run(queue_lock);
+        dequeue(queue_lock);
 
         post_loop();
     }
@@ -93,7 +165,7 @@ public:
         return pt.second;
     }
 
-    serial_instance_t(std::function<void (work_t&&)> executor) : _executor(executor) {
+    serial_instance_t(detail::executor_t&& executor) : _executor(std::move(executor)) {
     }
 
     ~serial_instance_t() {
@@ -106,7 +178,7 @@ public:
             if (_queue.empty())
                 break;
 
-            run(queue_lock);
+            dequeue(queue_lock);
         }
     }
 };
@@ -120,7 +192,7 @@ public:
 class serial_queue_t {
     std::shared_ptr<detail::serial_instance_t> _impl;
 
-  public:
+public:
     template <typename Executor>
     serial_queue_t(Executor e) : _impl(std::make_shared<detail::serial_instance_t>([_e = e](auto&& f){
         _e(std::move(f));
