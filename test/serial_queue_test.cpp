5d850c7c756eafee811b53f313f38d9662337529
Foster Brereton <fbrereto@adobe.com>
2017-06-07 14:01:29 -0700

    adding another test

--- test/serial_queue_test.cpp
+++ test/serial_queue_test.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string>
 
 #include <stlab/concurrency/serial_queue.hpp>
 #include <stlab/concurrency/default_executor.hpp>
 
 using namespace stlab;
 
-int main(int argc, const char * argv[]) {
+void test0() {
     serial_queue_t aq(stlab::default_executor);
     serial_queue_t bq(stlab::default_executor);
     serial_queue_t cq(stlab::default_executor);
@@ -60,6 +61,82 @@ int main(int argc, const char * argv[]) {
     dq([](){
         std::cout << "           d3           (12)\n";
     });
+}
+
+void test1() {
+    serial_queue_t         aq(stlab::default_executor);
+    serial_queue_t         bq(stlab::default_executor);
+    serial_queue_t         cq(stlab::default_executor);
+    serial_queue_t         dq(stlab::default_executor);
+    std::hash<std::string> str_hash;
+
+    std::size_t ahash(str_hash("a"));
+    std::size_t bhash(str_hash("b"));
+    std::size_t chash(str_hash("c"));
+    std::size_t dhash(str_hash("d"));
+
+    auto hash_combine([str_hash](std::size_t hash, const std::string& x){
+        return hash ^ (str_hash(x) << 1);
+    });
+
+    aq([&](){
+        ahash = hash_combine(ahash, "1");
+    });
+
+    bq([&](){
+        bhash = hash_combine(bhash, "1");
+    });
+
+    dq([&](){
+        dhash = hash_combine(dhash, "1");
+    });
+
+    bq([&](){
+        bhash = hash_combine(bhash, "2");
+    });
+
+    cq([&](){
+        chash = hash_combine(chash, "1");
+    });
+
+    cq([&](){
+        chash = hash_combine(chash, "2");
+    });
+
+    dq([&](){
+        dhash = hash_combine(dhash, "2");
+    });
+
+    cq([&](){
+        chash = hash_combine(chash, "3");
+    });
+
+    bq([&](){
+        bhash = hash_combine(bhash, "3");
+    });
+
+    aq([&](){
+        ahash = hash_combine(ahash, "2");
+    });
+
+    aq([&](){
+        ahash = hash_combine(ahash, "3");
+    });
+
+    dq([&](){
+        dhash = hash_combine(dhash, "3");
+    });
+
+    std::cout << "a: " << ahash << (ahash == std::size_t(14189587631650534178) ? " (OK)" : " (BAD) ") << '\n';
+    std::cout << "b: " << bhash << (bhash == std::size_t(11845100715899112661) ? " (OK)" : " (BAD) ") << '\n';
+    std::cout << "c: " << chash << (chash == std::size_t(8461684262208515064) ? " (OK)" : " (BAD) ") << '\n';
+    std::cout << "d: " << dhash << (dhash == std::size_t(17611461363091444353) ? " (OK)" : " (BAD) ") << '\n';
+}
+
+int main(int argc, const char * argv[]) {
+    test0();
+
+    test1();
 
     return 0;
 }
