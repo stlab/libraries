50407afde4cba5e7b219fc357207f0a6a34822ad
Foster Brereton <fbrereto@adobe.com>
2017-06-10 15:59:47 -0700

    better serial queue example

--- libraries/concurrency/serial_queue/serial_queue_example.cpp
+++ libraries/concurrency/serial_queue/serial_queue_example.cpp
@@ -9,32 +9,31 @@ using namespace std;
 using namespace stlab;
 
 serial_queue_t io_q(default_executor);
+atomic<bool>   done{false};
 
 void say_hello(string to_whom) {
-    auto f = io_q([_whom = move(to_whom)](){
+    io_q.executor()([_whom = move(to_whom)](){
         cout << "Hello, " << _whom << "!\n";
+    });
+}
+
+int main(int, char**) {
+    say_hello("fella");
+    say_hello("chief");
+    say_hello("pal");
+    say_hello("world");
+    auto f = io_q([&_done = done](){
+        _done = true;
     });
 
     while (!f.get_try()) { }
 }
 
-int main(int, char**) {
-    auto t0 = thread(&say_hello, "fella");
-    auto t1 = thread(&say_hello, "chief");
-    auto t2 = thread(&say_hello, "pal");
-    auto t3 = thread(&say_hello, "world");
-
-    t0.join();
-    t1.join();
-    t2.join();
-    t3.join();
-}
-
 /*
-    Result: (Order of outputs may change)
+    Result:
 
         Hello, fella!
-        Hello, world!
         Hello, chief!
         Hello, pal!
+        Hello, world!
 */
