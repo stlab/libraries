15b1701c213d9e4ab314f42cde3c0dcbe5a94b76
Foster Brereton <fbrereto@adobe.com>
2017-06-10 22:55:24 -0700

    the build script now pulls the associated stlab branch

--- build.sh
+++ build.sh
@@ -21,17 +21,15 @@ if [ -z "$TRAVIS_BRANCH" ]; then
 fi
 
 if [ ! -d stlab ]; then
-    echo "Cloning stlab branch $TRAVIS_BRANCH..."
+    echo "Cloning stlab/$TRAVIS_BRANCH..."
 
     git clone --branch $TRAVIS_BRANCH --depth=1 https://github.com/stlab/libraries.git stlab
 else
-    echo "Found stlab already."
-    echo "WARNING: "
-    echo "WARNING: The git branch may not be correct."
-    echo "WARNING: "
+    echo "Found stlab. Pulling $TRAVIS_BRANCH..."
 
     cd stlab
-    git pull
+    git co -b $TRAVIS_BRANCH
+    git pull origin $TRAVIS_BRANCH
     cd ..
 fi
 
@@ -67,12 +65,11 @@ do
     exit "$RETVAL"
   fi
 
-  echo "Running..."
-
   ./build/a.out > /dev/null
 
   export RETVAL=$?
   if [ "$RETVAL" != "0" ]; then
+    echo "Error: execution failure."
     exit "$RETVAL"
   fi
 done
