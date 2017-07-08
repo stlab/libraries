7fd58cac3bc5ec8178326583da1f6e822b0d168f
Foster Brereton <fbrereto@adobe.com>
2017-07-07 23:40:53 -0700

    adding mutex wrapping to the test helpers as per Felix's changes.

--- test/channel_test_helper.cpp
+++ test/channel_test_helper.cpp
@@ -9,4 +9,6 @@
 #include "channel_test_helper.hpp"
 
 std::queue<std::function<void()>> manual_scheduler::_tasks;
-std::atomic_int timed_sum::_x{ 0 };
+std::mutex                        manual_scheduler::_mutex;
+
+std::atomic_int                   timed_sum::_x{ 0 };
