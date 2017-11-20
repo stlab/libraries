ab7efcf73286aa9c412ed901f52bfb553de5b7ba
Felix Petriconi <felix@petriconi.net>
2017-11-20 18:23:00 +0100

    Correcting example

--- libraries/for_each_argument_example.cpp
+++ libraries/for_each_argument_example.cpp
@@ -4,6 +4,7 @@
 
 #include <stlab/concurrency/future.hpp>
 #include <stlab/concurrency/default_executor.hpp>
+#include <stlab/concurrency/immediate_executor.hpp>
 #include <stlab/concurrency/utility.hpp>
 
 using namespace stlab;
@@ -12,7 +13,7 @@ using namespace std;
 int main(int, char**) {
     auto fi = []{
         static std::size_t count_s{0};
-        return make_ready_future<int>(count_s++);
+        return make_ready_future<int>(count_s++, immediate_executor);
     };
 
     auto f = when_all(default_executor, [](auto... args){
@@ -27,4 +28,8 @@ int main(int, char**) {
         0
         1
         2
+    or
+        2
+        1
+        0
 */
