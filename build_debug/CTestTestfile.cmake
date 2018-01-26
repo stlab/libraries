a5f9478ed3d4750084946c8636a52cb50c381bfa
Felix Petriconi <felix@petriconi.net>
2018-01-26 19:17:55 +0100

    Next attempt to solve the AppVeyor issue

--- build_debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/misc/GitHub/libraries
-# Build directory: D:/misc/GitHub/libraries/build_debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
