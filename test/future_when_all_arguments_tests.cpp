d812263635f966e153e15ec9a037ec4b209bfd06
Felix Petriconi <felix@petriconi.net>
2018-07-28 15:23:39 +0200

    Correcting execution of ready when_all/when_any on the correct executor

--- test/future_when_all_arguments_tests.cpp
+++ test/future_when_all_arguments_tests.cpp
@@ -54,6 +54,46 @@ BOOST_AUTO_TEST_CASE(future_when_all_args_int_with_many_elements) {
     BOOST_REQUIRE_LE(4, custom_scheduler<0>::usage_counter());
     BOOST_REQUIRE_LE(1, custom_scheduler<1>::usage_counter());
 }
+
+BOOST_AUTO_TEST_CASE(future_when_all_args_int_with_ready_element) {
+    BOOST_TEST_MESSAGE("running future when_all int with ready element");
+
+    sut = when_all(custom_scheduler<1>(), [](auto x) { return x + x; }, make_ready_future<int>(42, immediate_executor));
+
+    check_valid_future(sut);
+    wait_until_future_completed(sut);
+
+    BOOST_REQUIRE_EQUAL(42 + 42, *sut.get_try());
+    BOOST_REQUIRE_LE(1, custom_scheduler<1>::usage_counter());
+}
+
+BOOST_AUTO_TEST_CASE(future_when_all_args_int_with_two_ready_element) {
+    BOOST_TEST_MESSAGE("running future when_all int with two ready element");
+
+    sut = when_all(custom_scheduler<1>(), [](auto x, auto y) { return x + y; },
+                   make_ready_future<int>(42, immediate_executor),
+                   make_ready_future<int>(42, immediate_executor));
+
+    check_valid_future(sut);
+    wait_until_future_completed(sut);
+
+    BOOST_REQUIRE_EQUAL(42 + 42, *sut.get_try());
+    BOOST_REQUIRE_LE(1, custom_scheduler<1>::usage_counter());
+}
+
+BOOST_AUTO_TEST_CASE(future_when_all_args) {
+
+    auto main_thread_id = std::this_thread::get_id();
+    auto sut = when_all(custom_scheduler<1>(), [] { 
+        return std::this_thread::get_id(); 
+    }, make_ready_future(stlab::immediate_executor));
+
+    wait_until_future_completed(sut);
+
+    BOOST_REQUIRE(main_thread_id != *sut.get_try());
+    BOOST_REQUIRE_LE(1, custom_scheduler<1>::usage_counter());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 BOOST_FIXTURE_TEST_SUITE(future_when_all_args_string, test_fixture<std::string>)
