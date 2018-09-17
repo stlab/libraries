83281cdf630f2e85626c0634acdd61c1149d79bc
Raoul Wols <raoulwols@gmail.com>
2018-09-17 12:38:12 +0200

    Do not find the boost test framework when importing stlab from another project

--- cmake/stlabConfig.cmake
+++ cmake/stlabConfig.cmake
@@ -1,4 +1,3 @@
 include( CMakeFindDependencyMacro )
-find_dependency( Boost 1.60.0 REQUIRED COMPONENTS unit_test_framework )
 find_package(Threads REQUIRED)
 include( "${CMAKE_CURRENT_LIST_DIR}/stlabTargets.cmake" )
