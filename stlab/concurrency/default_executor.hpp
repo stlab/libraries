3ff8c96dcb16dc729fb28ad78e2b32279eedaf76
Aaron Albers <aalbers@jstar.ai>
2018-10-10 09:28:08 -0600

    Check for ARC
    - If ARC is enabled in Xcode then `dispatch_release()`
      will not compile

--- stlab/concurrency/default_executor.hpp
+++ stlab/concurrency/default_executor.hpp
@@ -63,7 +63,9 @@ private:
         dispatch_group_t _group = dispatch_group_create();
         ~group() {
             dispatch_group_wait(_group, DISPATCH_TIME_FOREVER);
+#if !__has_feature(objc_arc)
             dispatch_release(_group);
+#endif
         }
     };
 
