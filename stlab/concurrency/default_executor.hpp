1d41158bf97c6f31ac7cd950b8f7c32a886eeff6
Aaron Albers <aaroncalbers@gmail.com>
2018-11-03 16:34:27 -0600

    _pool was checked twice rather than the new _cleanupgroup

--- stlab/concurrency/default_executor.hpp
+++ stlab/concurrency/default_executor.hpp
@@ -144,7 +144,7 @@ class task_system {
             if (_pool == nullptr) throw std::bad_alloc();
             
             _cleanupgroup = CreateThreadpoolCleanupGroup();
-            if (_pool == nullptr) throw std::bad_alloc();
+            if (_cleanupgroup == nullptr) throw std::bad_alloc();
             
             SetThreadpoolCallbackPool(&_callBackEnvironment, _pool);
             SetThreadpoolCallbackCleanupGroup(&_callBackEnvironment, _cleanupgroup, nullptr);
