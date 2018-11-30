f49f2fdad6f553491c3e7091aecca7331d2361be
Felix Petriconi <felix@petriconi.net>
2018-11-30 11:01:14 +0100

    Use correct paths for the patch file

--- .travis/patches/patch_std_variant.sh
+++ .travis/patches/patch_std_variant.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+HERE=`pwd`
+
 if [ ! -d /usr/lib/gcc/x86_64-linux-gnu/7.3.0/../../../../include/c++/7.3.0/ ]; then
     echo "You must have a destination directory (gcc 7.3.0 includes) in order to patch!"
     exit 1;
 fi
 
-cd /usr/lib/gcc/x86_64-linux-gnu/7.3.0/../../../../include/c++/7.3.0/
+pushd /usr/lib/gcc/x86_64-linux-gnu/7.3.0/../../../../include/c++/7.3.0/
 
-patch -uN -p0 -g0 < "std_variant.patch" && echo "Patching of std.variant succeeded. Happy coding!"
+patch -uN -p0 -g0 < "$HERE/.travis/patches/std_variant.patch" && echo "Patching of std.variant succeeded. Happy coding!"
+
+popd
 
 exit 0
