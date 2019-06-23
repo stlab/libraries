84f009432b39be60e261a2f0be814ee1311a9907
Aaron Albers <aaroncalbers@gmail.com>
2019-06-23 12:59:14 -0600

    Take Felix changes

--- stlab/concurrency/router.hpp
+++ stlab/concurrency/router.hpp
@@ -1,7 +1,7 @@
 /*
-    Copyright 2015 Adobe
-    Distributed under the Boost Software License, Version 1.0.
-    (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+Copyright 2015 Adobe
+Distributed under the Boost Software License, Version 1.0.
+(See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 */
 
 /**************************************************************************************************/
@@ -10,10 +10,8 @@
 #define STLAB_CONCURRENCY_ROUTER_HPP
 
 #include <algorithm>
-#include <memory>
 #include <utility>
 #include <vector>
-#include <stdexcept>
 
 #include <stlab/concurrency/channel.hpp>
 
@@ -24,127 +22,161 @@ namespace stlab {
 /**************************************************************************************************/
 
 inline namespace v1 {
-
 /**************************************************************************************************/
 
-template <class T, class K>
-class router {
-    using channel_t = std::pair<sender<T>, receiver<T>>;
-    using route_pair = std::pair<const K, channel_t>;
-    using routes = std::vector<route_pair>;
+template <typename T>
+using channel_t = std::pair<sender<T>, receiver<T>>;
 
-    template<std::size_t N>
-    using static_routes = std::array<route_pair, N>;
+namespace detail {
+
+template <typename T, typename K, typename E, typename F>
+class shared_router : public std::enable_shared_from_this<shared_router<T, K, E, F>> {
+    using route_pair_t = std::pair<K, channel_t<T>>;
+    E _executor; // of the router function
+    F _router_function;
+    std::vector<route_pair_t> _routes;
+    bool _ready = false;
 
 public:
-    template <class I, class F>
-    router(const std::pair<I, I>& key_range, F router_func) : _self{std::make_unique<model<routes, F>>(make_routes(key_range), std::move(router_func))} {}
+    shared_router() = default;
+    ~shared_router() = default;
+    shared_router(shared_router&&) = default;
+    shared_router(const shared_router&) = default;
+    shared_router& operator=(shared_router&&) = default;
+    shared_router& operator=(const shared_router&) = default;
 
-    template <std::size_t N, class E, class F>
-    router(const std::array<std::pair<K, E>, N>& route_keys, F router_func) : _self{std::make_unique<model<static_routes<N>, F>>(make_routes(route_keys), std::move(router_func))} {}
+    shared_router(E executor, F router_function) :
+        _executor{executor}, _router_function(std::move(router_function)) {}
 
-    void set_ready() { _self->set_ready(); }
-    receiver<T> get_route(const K& key) const { return _self->get_route(key); }
-    void operator()(T t) { _self->route(std::move(t)); }
+    template <typename... U>
+    shared_router(E executor, F router_function, std::pair<K, channel_t<U>>... route_pairs) :
+        _executor{std::move(executor)},
+        _router_function(std::move(router_function)), _routes{
+                                                          std::make_pair(route_pairs.first,
+                                                                         route_pairs.second)...} {
+        assert(std::is_sorted(_routes.cbegin(), _routes.cend(),
+                              [](const auto& x, const auto& y) { return x.first < y.first; }));
+        set_ready();
+    }
 
-private:
-    template<class I>
-    static routes make_routes(const std::pair<I, I>& range) {
-        routes result;
-        std::transform(range.first, range.second, std::back_inserter(result), [](auto route_key) -> route_pair {
-            return {std::get<0>(route_key), channel<T>(std::get<1>(route_key))};
-        });
-        if (std::empty(result)) throw std::runtime_error("no routes provided");
+    void set_ready() {
+        for (auto& route : _routes)
+            route.second.second.set_ready();
+        _ready = true;
+    }
+
+    stlab::optional<receiver<T>> route(K key) const {
+        stlab::optional<receiver<T>> result;
+
+        auto find_it = std::lower_bound(_routes.begin(), _routes.end(), key,
+                                        [](const auto& p, const auto& k) { return p.first < k; });
+
+        if (find_it == _routes.end() || find_it->first != key) return result;
+        result = find_it->second.second;
+
         return result;
     }
 
-    template<std::size_t N>
-    using mutable_static_routes = std::array<std::pair<K, channel_t>, N>;
+    template <typename Ex>
+    receiver<T> add_route(K key, Ex executor) {
+        assert(!_ready);
 
-    template<std::size_t N, std::size_t... Is>
-    static static_routes<N> make_static_routes(mutable_static_routes<N> mutable_routes, std::index_sequence<Is...>) {
-        return {{ {std::move(std::get<0>(mutable_routes[Is])), std::move(std::get<1>(mutable_routes[Is]))}... }};
+        channel_t<T> ch = channel<T>(executor);
+        auto result = ch.second;
+        auto insert_it = std::lower_bound(_routes.begin(), _routes.end(), key,
+                                          [](const auto& p, const auto& k) { return p.first < k; });
+
+        assert(insert_it == _routes.end() || insert_it->first != key);
+
+        _routes.insert(insert_it, std::make_pair(key, std::move(ch)));
+
+        return result;
     }
 
-    template<std::size_t N, std::size_t... Is>
-    static static_routes<N> make_static_routes(mutable_static_routes<N> mutable_routes) {
-        return make_static_routes(std::move(mutable_routes), std::make_index_sequence<N>());
-    }
+    receiver<T> add_route(K key) { return add_route(std::move(key), _executor); }
 
-    template<std::size_t N, class E>
-    static static_routes<N> make_routes(const std::array<std::pair<K, E>, N>& route_keys) {
-        mutable_static_routes<N> result;
-        std::transform(std::begin(route_keys), std::end(route_keys), std::begin(result), [](auto route_key) -> route_pair {
-            return {std::get<0>(route_key), channel<T>(std::get<1>(route_key))};
-        });
-        return make_static_routes(std::move(result));
-    }
-
-    struct concept_t {
-        virtual receiver<T> get_route(const K& key) const = 0;
-        virtual void set_ready() = 0;
-        virtual void route(T t) = 0;
-        virtual ~concept_t() = default;
-    };
-
-    template <class R, class F>
-    struct model : concept_t {
-        model(R routes, F router_func)
-        : _routes{std::move(routes)}
-        , _router_func{std::move(router_func)}
-        {}
-
-        void set_ready() override {
-            for (auto& pair : _routes) pair.second.second.set_ready();
-        }
-
-        receiver<T> get_route(const K& key) const override {
-            auto find_it = std::lower_bound(std::begin(_routes), std::end(_routes), key, [](const route_pair& a, const K& k){
-                return a.first < k;
-            });
-            if (find_it == std::end(_routes) || find_it->first != key) throw std::runtime_error("no such route");
-            return find_it->second.second;
-        }
-
-        void route(T t) override {
-            const auto& keys = _router_func(t);
-            if (std::empty(keys)) return;
-            route(keys, std::move(t));
-        }
-
-        template<class C, typename std::enable_if_t<std::is_same<typename C::value_type, std::pair<K, bool>>::value, int> = 0>
-        void route(const C& keys, T t) {
-            auto find_it = std::begin(_routes);
+    void operator()(T arg) {
+        assert(_ready);
+        _executor([_weak_this = make_weak_ptr(this->shared_from_this()), _arg = std::move(arg)] {
+            auto _this = _weak_this.lock();
+            if (!_this) return;
+            auto keys = _this->_router_function(_arg);
+            auto find_it = std::begin(_this->_routes);
             for (const auto& key : keys) {
-                if (!key.second) continue;
-                find_it = std::lower_bound(find_it, std::end(_routes), key, [](const route_pair& a, const std::pair<K, bool>& k){
-                    return a.first < k.first;
-                });
-                if (find_it == std::end(_routes)) break;
-                if (find_it->first != key.first) continue;
-                find_it->second.first(t);
-            }
-        }
-
-        template<class C, typename std::enable_if_t<std::is_same<typename C::value_type, K>::value, int> = 0>
-        void route(const C& keys, T t) {
-            auto find_it = std::begin(_routes);
-            for (const auto& key : keys) {
-                find_it = std::lower_bound(find_it, std::end(_routes), key, [](const route_pair& a, const K& k){
-                    return a.first < k;
-                });
-                if (find_it == std::end(_routes)) return;
+                find_it =
+                    std::lower_bound(find_it, std::end(_this->_routes), key,
+                                     [](const auto& p, const auto& k) { return p.first < k; });
+                if (find_it == std::end(_this->_routes)) return;
                 if (find_it->first != key) continue;
-                find_it->second.first(t);
+                find_it->second.first(_arg);
             }
-        }
-
-        F _router_func;
-        R _routes;
-    };
-
-    std::unique_ptr<concept_t> _self;
+        });
+    }
 };
+
+} // namespace detail
+
+/**************************************************************************************************/
+
+template <typename T, typename K, typename E, typename F>
+class router {
+    using ptr_t = std::shared_ptr<detail::shared_router<T, K, E, F>>;
+    ptr_t _p;
+
+public:
+    router() = default;
+    ~router() = default;
+
+    void swap(router& x) noexcept { std::swap(_p, x._p); }
+
+    inline friend void swap(router& x, router& y) { x.swap(y); }
+    inline friend bool operator==(const router& x, const router& y) {
+        return (x._p && y._p && *x._p == *y._p) || (!x._p && !y._p);
+    }
+    inline friend bool operator!=(const router& x, const router& y) { return !(x == y); }
+
+    bool valid() const { return static_cast<bool>(_p); }
+
+    router(router&&) = default;
+    router& operator=(router&&) = default;
+
+    router(const router& x) : _p(std::make_shared<detail::shared_router<T, K, E, F>>(*x._p)) {}
+
+    router& operator=(const router& x) {
+        auto tmp = x;
+        *this = std::move(tmp);
+        return *this;
+    }
+
+    router(E executor, F router_function) :
+        _p(std::make_shared<detail::shared_router<T, K, E, F>>(std::move(executor),
+                                                               std::move(router_function))) {}
+
+    template <typename... U>
+    router(E executor, F router_function, std::pair<K, channel_t<U>>... route_pairs) :
+        _p(std::make_shared<detail::shared_router<T, K, E, F>>(
+            std::move(executor),
+            std::move(router_function),
+            std::make_pair(route_pairs.first, route_pairs.second)...)) {}
+
+    void set_ready() { _p->set_ready(); }
+
+    stlab::optional<receiver<T>> route(K key) const { return _p->route(std::move(key)); }
+
+    template <typename Ex>
+    receiver<T> add_route(K key, Ex executor) {
+        return _p->add_route(std::move(key), std::move(executor));
+    }
+
+    receiver<T> add_route(K key) { return _p->add_route(std::move(key)); }
+
+    void operator()(T arg) { _p->operator()(std::move(arg)); }
+};
+
+template <typename T, typename K, typename E, typename F>
+auto make_router(E executor, F router_function) {
+    return router<T, K, E, F>(std::move(executor), std::move(router_function));
+}
 
 /**************************************************************************************************/
 
