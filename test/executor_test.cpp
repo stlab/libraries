b001ff363e327f2b8d16adef045bd4ac0f3ca5fe
Felix Petriconi <felix@petriconi.net>
2019-06-27 22:11:46 +0200

    Start with low, default, high executor tests

--- test/executor_test.cpp
+++ test/executor_test.cpp
@@ -6,8 +6,88 @@
 /**************************************************************************************************/
 
 #include <stlab/concurrency/default_executor.hpp>
+#include <stlab/concurrency/serial_queue.hpp>
 #include <boost/test/unit_test.hpp>
 
-BOOST_AUTO_TEST_CASE(executor_test) {
+#include <thread>
 
+using namespace stlab;
+
+BOOST_AUTO_TEST_CASE(all_low_prio_tasks_get_executed) {
+
+    serial_queue_t queue(low_executor);
+    std::vector<int> results;
+    std::atomic_bool done{false};
+
+    for (auto i = 0; i < 10; ++i)
+    {
+        queue.executor()([_i = i, &results]{
+            results.push_back(_i);
+        });
+    }
+    queue.executor()([&done]{
+        done = true;
+    });
+
+    while (!done) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    for (auto i = 0; i < 10; ++i)
+    {
+        BOOST_REQUIRE_EQUAL(i, results[i]);
+    }
 }
+
+BOOST_AUTO_TEST_CASE(all_default_prio_tasks_get_executed) {
+
+    serial_queue_t queue(default_executor);
+    std::vector<int> results;
+    std::atomic_bool done{false};
+
+    for (auto i = 0; i < 10; ++i)
+    {
+        queue.executor()([_i = i, &results]{
+            results.push_back(_i);
+        });
+    }
+    queue.executor()([&done]{
+        done = true;
+    });
+
+    while (!done) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    for (auto i = 0; i < 10; ++i)
+    {
+        BOOST_REQUIRE_EQUAL(i, results[i]);
+    }
+}
+
+BOOST_AUTO_TEST_CASE(all_high_prio_tasks_get_executed) {
+
+    serial_queue_t queue(high_executor);
+    std::vector<int> results;
+    std::atomic_bool done{false};
+
+    for (auto i = 0; i < 10; ++i)
+    {
+        queue.executor()([_i = i, &results]{
+            results.push_back(_i);
+        });
+    }
+    queue.executor()([&done]{
+        done = true;
+    });
+
+    while (!done) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+
+    for (auto i = 0; i < 10; ++i)
+    {
+        BOOST_REQUIRE_EQUAL(i, results[i]);
+    }
+}
+
