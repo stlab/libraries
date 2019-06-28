236a9ff10cf3a33e53d3bd4db0f61c49ced5253c
Felix Petriconi <felix@petriconi.net>
2019-06-28 11:26:41 +0200

    Correct test expectation

--- test/executor_test.cpp
+++ test/executor_test.cpp
@@ -121,7 +121,7 @@ BOOST_AUTO_TEST_CASE(all_tasks_will_be_executed_according_to_their_prio) {
         lowSum += results[i + iterations * 2];
     }
 
-    BOOST_REQUIRE(3.0 - static_cast<double>(highSum) / iterations < 0.001);
-    BOOST_REQUIRE(2.0 - static_cast<double>(defaultSum) / iterations < 0.001);
-    BOOST_REQUIRE(1.0 - static_cast<double>(lowSum) / iterations < 0.001);
+    BOOST_REQUIRE(abs(3.0 - static_cast<double>(highSum) / iterations) < 0.001);
+    BOOST_REQUIRE(abs(2.0 - static_cast<double>(defaultSum) / iterations) < 0.001);
+    BOOST_REQUIRE(abs(1.0 - static_cast<double>(lowSum) / iterations) < 0.001);
 }
