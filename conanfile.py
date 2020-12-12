d9615afa1dfb0ecbaf268b6e6e65a75cc2e111fc
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-12-12 16:05:31 -0300

    configure Boost 1.74.0

--- conanfile.py
+++ conanfile.py
@@ -46,36 +46,36 @@ class StlabLibrariesConan(ConanFile):
         self.options["boost"].without_system=False
         self.options["boost"].without_timer=False
         self.options["boost"].without_test=False
-        self.options["boost"].without_iostreams=True
-        self.options["boost"].without_log=True
-        self.options["boost"].without_regex=True
-        self.options["boost"].without_locale=True
-        self.options["boost"].without_exception=True
-        self.options["boost"].without_filesystem=True
-        self.options["boost"].without_container=True
-        self.options["boost"].without_program_options=True
-        self.options["boost"].without_wave=True
-        self.options["boost"].without_thread=True
-        self.options["boost"].without_graph_parallel=True
-        self.options["boost"].without_context=True
-        self.options["boost"].without_random=True
-        self.options["boost"].without_graph=True
-        self.options["boost"].without_serialization=True
-        self.options["boost"].without_date_time=True
-        self.options["boost"].without_fiber=True
-        self.options["boost"].without_coroutine=True
-        self.options["boost"].without_mpi=True
-        self.options["boost"].without_type_erasure=True
-        self.options["boost"].without_math=True
-        self.options["boost"].without_container=True
-        self.options["boost"].without_log=True
-        self.options["boost"].without_exception=True
-        self.options["boost"].without_python=True
-        self.options["boost"].without_stacktrace=True
-        self.options["boost"].without_atomic=True
-        
-        # self.options["spdlog"].header_only = True
 
+        self.options["boost"].without_atomic = True
+        self.options["boost"].without_container = True
+        self.options["boost"].without_context = True
+        self.options["boost"].without_contract = True
+        self.options["boost"].without_coroutine = True
+        self.options["boost"].without_date_time = True
+        self.options["boost"].without_exception = True
+        self.options["boost"].without_fiber = True
+        self.options["boost"].without_filesystem = True
+        self.options["boost"].without_graph = True
+        self.options["boost"].without_graph_parallel = True
+        self.options["boost"].without_headers = True
+        self.options["boost"].without_iostreams = True
+        self.options["boost"].without_locale = True
+        self.options["boost"].without_log = True
+        self.options["boost"].without_math = True
+        self.options["boost"].without_mpi = True
+        self.options["boost"].without_nowide = True
+        self.options["boost"].without_program_options = True
+        self.options["boost"].without_python = True
+        self.options["boost"].without_random = True
+        self.options["boost"].without_regex = True
+        self.options["boost"].without_serialization = True
+        self.options["boost"].without_stacktrace = True
+        self.options["boost"].without_thread = True
+        self.options["boost"].without_type_erasure = True
+        self.options["boost"].without_wave = True
+
+        # self.options["spdlog"].header_only = True
 
     def build(self):
         cmake = CMake(self)
