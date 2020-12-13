5a8ffbb06c6e540985e8e9c49bd24bd257ef6cb9
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-12-12 23:10:10 -0300

    uses GCC 10

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(build_policy = "missing", archs = ["x86_64"], apple_clang_versions= ["12.0"])
+    builder = ConanMultiPackager(build_policy = "missing", archs = ["x86_64"])
     builder.add_common_builds()
     builder.run()
 
