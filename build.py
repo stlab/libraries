39f671439b13f05eafc64049adc79f03150d8f65
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-01-12 17:39:34 -0300

    Test activated by default in build script

--- build.py
+++ build.py
@@ -1,7 +1,7 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(build_policy = "missing", archs = ["x86_64"])
+    builder = ConanMultiPackager(build_policy = "missing", archs = ["x86_64"], options = ["stlab:testing=True"])
         # apple_clang_versions= ["12.0"]
     builder.add_common_builds()
     builder.run()
