7446df349349af8aa9204e9b3bb33f7bb5eef8f2
Felix Petriconi <felix@petriconi.net>
2021-02-09 10:43:45 +0100

    Fix default test argument

--- cmake/RunTests.cmake
+++ cmake/RunTests.cmake
@@ -1,5 +1,5 @@
 if(NOT DEFINED ENV{TESTS_ARGUMENTS})
-    set(ENV{TESTS_ARGUMENTS} "--default-arguments")
+    set(ENV{TESTS_ARGUMENTS} "")
 endif()
 execute_process(COMMAND ${TEST_EXECUTABLE} $ENV{TESTS_ARGUMENTS} RESULT_VARIABLE result)
 if(NOT "${result}" STREQUAL "0")
