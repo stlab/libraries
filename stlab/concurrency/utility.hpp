51b9f6fdc6bc615bf4135e4c114a5e6644312a2d
Sean Parent <sparent@adobe.com>
2021-03-12 14:17:12 -0800

    blocking_get_for() added
    Fix for make_exceptional_future<void> and improvements
    blocking_get_for added
    deprecated blocking_get() with timeout

--- stlab/concurrency/utility.hpp
+++ stlab/concurrency/utility.hpp
@@ -19,7 +19,6 @@
 #include <stlab/concurrency/default_executor.hpp>
 #include <stlab/concurrency/future.hpp>
 #include <stlab/concurrency/immediate_executor.hpp>
-#include <stlab/concurrency/optional.hpp>
 
 /**************************************************************************************************/
 
@@ -41,20 +40,6 @@ namespace stlab {
 /**************************************************************************************************/
 
 inline namespace v1 {
-namespace detail {
-
-struct shared_state {
-    bool flag{false};
-    std::condition_variable condition;
-    std::mutex m;
-    std::exception_ptr error{nullptr};
-};
-
-template <typename T>
-struct shared_state_result : shared_state {
-    stlab::optional<T> result;
-};
-} // namespace detail
 
 /**************************************************************************************************/
 
@@ -73,23 +58,34 @@ future<void> make_ready_future(E executor) {
     return p.second;
 }
 
+namespace detail {
+
+template <class T>
+struct _make_exceptional_future {
+    template <typename E>
+    future<T> operator()(std::exception_ptr error, E executor) const {
+        auto p = package<T(T)>(std::move(executor),
+                               [](auto&& a) { return std::forward<decltype(a)>(a); });
+        p.first.set_exception(std::move(error));
+        return std::move(p.second);
+    }
+};
+
+template <>
+struct _make_exceptional_future<void> {
+    template <typename E>
+    future<void> operator()(std::exception_ptr error, E executor) const {
+        auto p = package<void()>(std::move(executor), []() {});
+        p.first.set_exception(std::move(error));
+        return std::move(p.second);
+    }
+};
+
+} // namespace detail
+
 template <typename T, typename E>
 future<T> make_exceptional_future(std::exception_ptr error, E executor) {
-    auto p = package<T(T)>(std::move(executor), [_error = error](auto&& x) {
-        std::rethrow_exception(_error);
-        return std::forward<decltype(x)>(x);
-    });
-    p.first(T{});
-    return std::move(p.second);
-}
-
-template <typename E>
-future<void> make_exceptional_future(std::exception_ptr error, E executor) {
-    auto p = package<void()>(std::move(executor), [_error = error]() {
-        std::rethrow_exception(_error);
-    });
-    p.first();
-    return p.second;
+    return detail::_make_exceptional_future<T>{}(std::move(error), std::move(executor));
 }
 
 namespace detail {
@@ -166,66 +162,53 @@ T blocking_get(future<T> x) {
     return detail::_get_ready_future<T>{}(std::move(x));
 }
 
-#if 0
-template <typename T>
-auto blocking_get(future<T> x, const std::chrono::nanoseconds& timeout) {
-    future<T> guarded;
-    std::condition_variable condition;
-    std::mutex m;
-
-    auto hold = std::move(x).recover(immediate_executor, [&](auto&& r) {
-        std::unique_lock<std::mutex> lock{m};
-        guarded = std::forward<decltype(r)>(r);
-        condition.notify_one();
-    });
-
-    bool valid;
-    {
-        std::unique_lock<std::mutex> lock{m};
-        valid = condition.wait_for(lock, timeout, [&] { return guarded.valid(); });
-    }
-
-    return valid ? guarded.get_try() : decltype(guarded.get_try()){};
-}
-#endif
-
 namespace detail {
 
 template <class T>
 struct blocking_get_guarded {
+    bool _timed_out{false};
     future<T> _result;
     std::condition_variable _condition;
     std::mutex _mutex;
 
     template <class F>
-    void set(F&& r) {
+    auto set(F&& r) -> future<T> {
         std::unique_lock<std::mutex> lock{_mutex};
+        if (_timed_out) return std::forward<F>(r);
         _result = std::forward<F>(r);
         _condition.notify_one();
+        return make_exceptional_future<T>(
+            std::make_exception_ptr(std::runtime_error("not an error")), immediate_executor);
     }
 
-    auto wait_for(const std::chrono::nanoseconds& timeout) {
-        bool valid;
-        {
-            std::unique_lock<std::mutex> lock{_mutex};
-            valid = _condition.wait_for(lock, timeout, [&] { return _result.valid(); });
-        }
-        return valid ? _result.get_try() : decltype(_result.get_try()){};
+    auto wait_for(const std::chrono::nanoseconds& timeout) -> future<T> {
+        std::unique_lock<std::mutex> lock{_mutex};
+        _timed_out = !_condition.wait_for(lock, timeout, [&] { return _result.valid(); });
+        return _timed_out ? future<T>{} : std::move(_result);
     }
 };
 
 } // namespace detail
 
-template <typename T>
-auto blocking_get(future<T> x, const std::chrono::nanoseconds& timeout) {
+template <class T>
+auto blocking_get_for(future<T> x, const std::chrono::nanoseconds& timeout) -> future<T> {
     auto p = std::make_shared<detail::blocking_get_guarded<T>>();
 
     auto hold = std::move(x).recover(
         immediate_executor, [_p = std::weak_ptr<detail::blocking_get_guarded<T>>(p)](auto&& r) {
-            if (auto p = _p.lock()) p->set(std::forward<decltype(r)>(r));
+            if (auto p = _p.lock()) return p->set(std::forward<decltype(r)>(r));
+            return make_exceptional_future<T>(
+                std::make_exception_ptr(std::runtime_error("not an error")), immediate_executor);
         });
 
-    return p->wait_for(timeout);
+    auto result = p->wait_for(timeout);
+    return result.valid() ? std::move(result) : std::move(hold);
+}
+
+template <class T>
+[[deprecated("Use blocking_get_for instead.")]] auto blocking_get(
+    future<T> x, const std::chrono::nanoseconds& timeout) -> decltype(x.get_try()) {
+    return blocking_get_for(std::move(x), timeout).get_try();
 }
 
 /**************************************************************************************************/
