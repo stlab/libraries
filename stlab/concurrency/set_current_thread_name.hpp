c86c645eb6696360b49a2ff05aa25aa07f5b94d2
Sean Parent <sean.parent@stlab.cc>
2021-04-09 10:50:56 -0700

    Sean parent/set thread name (#376)
    * Splitting out set_current_thread_name() into a  separate file
    
    The previous implementation did not work on wasm, I also added Windows support. This should make it simple to provide platform-specific implementations.

---
+++ stlab/concurrency/set_current_thread_name.hpp
@@ -0,0 +1,81 @@
+#ifndef STLAB_SET_CURRENT_THREAD_NAME_HPP
+#define STLAB_SET_CURRENT_THREAD_NAME_HPP
+
+/**************************************************************************************************/
+#if defined(__APPLE__)
+
+#include <pthread.h>
+
+/**************************************************************************************************/
+#elif defined(_MSC_VER)
+
+#include <cstring>
+#include <string>
+
+#include <windows.h>
+
+#include <processthreadsapi.h>
+#include <stringapiset.h>
+
+/**************************************************************************************************/
+#elif defined(__EMSCRIPTEN__) && defined(__EMSCRIPTEN_PTHREADS__)
+
+#include <pthread.h>
+
+#include <emscripten/threading.h>
+
+/**************************************************************************************************/
+#elif defined(__has_include) && __has_include(<pthread.h>)
+
+#include <pthread.h>
+
+/**************************************************************************************************/
+#endif
+
+/**************************************************************************************************/
+
+namespace stlab {
+
+/**************************************************************************************************/
+#if defined(__APPLE__)
+
+inline void set_current_thread_name(const char* name) { pthread_setname_np(name); }
+
+/**************************************************************************************************/
+#elif defined(_MSC_VER)
+
+inline void set_current_thread_name(const char* name) {
+    /* Should string->wstring be split out to a utility? */
+    int count = MultiByteToWideChar(CP_UTF8, 0, name, static_cast<int>(std::strlen(name)), NULL, 0);
+    if (count <= 0) return;
+    std::wstring str(count, wchar_t{});
+    count = MultiByteToWideChar(CP_UTF8, 0, name, static_cast<int>(std::strlen(name)), &str[0],
+                                static_cast<int>(str.size()));
+    if (count <= 0) return;
+
+    (void)SetThreadDescription(GetCurrentThread(), str.c_str());
+}
+
+/**************************************************************************************************/
+#elif defined(__EMSCRIPTEN__) && defined(__EMSCRIPTEN_PTHREADS__)
+
+inline void set_current_thread_name(const char* name) {
+    emscripten_set_thread_name(pthread_self(), name);
+}
+
+/**************************************************************************************************/
+#elif defined(__has_include) && __has_include(<pthread.h>)
+
+inline void set_current_thread_name(const char* name) { pthread_setname_np(pthread_self(), name); }
+
+/**************************************************************************************************/
+#else
+
+inline void set_current_thread_name(const char*) {}
+
+/**************************************************************************************************/
+#endif
+
+} // namespace stlab
+
+#endif
