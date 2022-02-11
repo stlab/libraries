152b8732f3234503c22fb625fe248dadf2e6916a
Felix Petriconi <felix@petriconi.net>
2022-02-11 20:29:13 +0100

    Update conan version because of expired certificate

--- .github/install.sh
+++ .github/install.sh
@@ -21,8 +21,8 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
 fi
 
 pip install --upgrade pip
-pip install conan_package_tools==0.35.0
-pip install conan==1.32.0
+pip install conan_package_tools==0.36.0
+pip install conan==1.41.0
 
 conan --version
 conan user
