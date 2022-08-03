aed1e3aa7492d6c0ede22cab6f6a26a1d8ccd224
Nick DeMarco <demarco@adobe.com>
2022-08-03 14:46:32 -0400

    Update generate_docs.sh to use new CMake build strategy

--- generate_docs.sh
+++ generate_docs.sh
@@ -1,19 +1,63 @@
 #!/bin/bash
 
-# The intent of this script is to generate the documentation shell that will eventually be migrated
-# to stlab.github.io. This script assumes `stlab/` and `stlab.github.io/` are sibling repositories.
+#
+# The intent of this script is to generate the documentation shell for stlab.github.io.
+# Note that stlab.github.io is a submodule of this repository, stored in /docs.
+# Also see .hyde-config.
+#
+
+#
+# Constants
+#
+
+CMAKE_BUILD_DIR=../BUILD-DOCS
+
+XCODE_TOOLCHAIN=$(xcode-select -p)
+XCODE_CPP_DIR=${XCODE_TOOLCHAIN}/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
+
+HYDE_EXECUTABLE=/Users/demarco/dev/hyde/build/hyde
+HYDE_ARGS="--hyde-update --access-filter-public --namespace-blacklist=detail,unsafe --use-system-clang"
+CLANG_ARGS="-I$XCODE_CPP_DIR -I$CMAKE_BUILD_DIR"
+
+#
+# User-specified variables
+#
+
+FORCE=0
+
+#
+# Argument Parsing
+#
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -f|--force)
+      FORCE=1
+      shift # past argument
+      ;;
+    -*|--*)
+      echo "Unknown option $1"
+      exit 1
+      ;;
+  esac
+done
+
+#
+# Invoke CMake, iff: 
+# - the build directory does not exist, or
+# - the user specified `--force`
+#
+
+if [ -d $CMAKE_BUILD_DIR ] && [ $FORCE -eq 0 ]
+then
+    echo "$CMAKE_BUILD_DIR already exists. Please delete it or use -f." 
+    exit 1
+else
+    cmake -S. -B $CMAKE_BUILD_DIR -GNinja -DCMAKE_CXX_STANDARD=17 -DCMAKE_BUILD_TYPE=Release
+fi
 
 pushd $(dirname $0) > /dev/null
 
-XCODE_TOOLCHAIN=$(xcode-select -p)
-
-# They moved it _again_! *shakes fist*
-XCODE_11_5_CPP_DIR=${XCODE_TOOLCHAIN}/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1
-
-# echo ${XCODE_11_5_CPP_DIR}
-
-HYDE_ARGS="--hyde-update --access-filter-public --namespace-blacklist=detail,unsafe --use-system-clang"
-
-hyde ${HYDE_ARGS} stlab/forest.hpp -- -I${XCODE_11_5_CPP_DIR}
+find stlab -name "*.hpp" -exec $HYDE_EXECUTABLE $HYDE_ARGS {} -- $CLANG_ARGS \;
 
 popd > /dev/null
