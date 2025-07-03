5c7601dd5014b2d1bcc2fa7681a9d1614a72f70b
Sean Parent <sparent@adobe.com>
2025-07-02 23:05:46 -0700

    Cleaning up comments

--- include/stlab/version.hpp
+++ include/stlab/version.hpp
@@ -11,17 +11,7 @@
 
 #include <stlab/config.hpp>
 
-
-
-//  STLAB_VERSION % 100 is the patch level
-//  STLAB_VERSION / 100 % 1000 is the minor version
-//  STLAB_VERSION / 100000 is the major version
-//
-//  STLAB_LIB_VERSION is a string in the form "x_y[_z]" where x is the major version
-//  number, y is the minor version number, and z is the patch level if not 0.
-//
 //  These version numbers are automatically generated from the CMake project version.
-//
 
 /*!
     `STLAB_VERSION % 100` is the patch level
