b1afeeb04abe8d41fd4a0d1b61396205b3c5cff1
Ben Medina <bmedina@adobe.com>
2025-08-14 14:49:40 -0700

    Consolidate on a single option: STLAB_TASK_POOL_SIZE. If STLAB_MINIMAL_TASK_POOL is provided, issue a warning, and force set STLAB_TASK_POOL_SIZE to 1.

--- include/stlab/concurrency/default_executor.hpp
+++ include/stlab/concurrency/default_executor.hpp
@@ -316,7 +316,7 @@ public:
 class priority_task_system {
     // Returns the number of hardware threads, or 1 if not available.
     static unsigned hardware_concurrency() {
-#if STLAB_TASK_POOL_CUSTOM_SIZE()
+#if STLAB_TASK_POOL_SIZE() > 0
         return STLAB_TASK_POOL_SIZE();
 #else
         return std::max(1u, std::thread::hardware_concurrency());
